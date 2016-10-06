5894b83156799f1eb51f3e0770c42ed0a9c0e486
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2016-10-05 23:01:42 -0300

    refactor decorate

--- fito/operations/decorate.py
+++ fito/operations/decorate.py
@@ -1,8 +1,5 @@
 from __future__ import print_function
 
-from functools import wraps
-from glob import glob
-import traceback
 import inspect
 import os
 
@@ -13,16 +10,13 @@ try:
     import cPickle
 except ImportError:
     import pickle as cPickle
-from filelock import FileLock
 
 caches_dir = os.path.join(os.path.dirname(__file__), 'caches')
 if not os.path.exists(caches_dir): os.makedirs(caches_dir)
 VERBOSE = False
 
 
-class as_operation(object):
-    data = {}
-
+class GenericDecorator(object):
     def __init__(self, **kwargs):
         """
         :param method_type: if is None, the decorated function is assumed to be a function, otherwise it is assumed
@@ -46,30 +40,46 @@ class as_operation(object):
 
         cls = type(instance) if instance is not None else owner
         assert cls is not None
-        return operation_from_func(
-            f=new_f,
-            out_type=self.out_type,
-            out_name=self.out_name,
-            args_specifications=self.args_specifications,
-            f_spec=inspect.getargspec(self.func),
-            method_type=self.method_type
-        )
+        return self.create_decorated(self.func, new_f, inspect.getargspec(self.func))
 
     def __call__(self, func):
         if self.method_type:
             self.func = func
             return self
         else:
-            return operation_from_func(
-                f=func,
-                out_type=self.out_type,
-                out_name=self.out_name,
-                args_specifications=self.args_specifications
-            )
+            return self.create_decorated(func, func)
+
+    def create_decorated(self, to_wrap, func_to_execute, f_spec=None):
+        raise NotImplementedError()
 
 
-def operation_from_func(f, out_type, out_name, args_specifications, f_spec=None, method_type=False):
-    f_spec = f_spec or inspect.getargspec(f)
+class as_operation(GenericDecorator):
+    def create_decorated(self, to_wrap, func_to_execute, f_spec=None):
+        f_spec = f_spec or inspect.getargspec(to_wrap)
+        return operation_from_func(
+            to_wrap=to_wrap,
+            func_to_execute=func_to_execute,
+            out_type=self.out_type,
+            out_name=self.out_name,
+            args_specifications=self.args_specifications,
+            f_spec=f_spec,
+            method_type=self.method_type
+        )
+
+
+def operation_from_func(to_wrap, func_to_execute, out_type, out_name, args_specifications, f_spec=None, method_type=False):
+    """
+    In the case of methods, to_wrap is not the same to func_to_execute
+    :param to_wrap:
+    :param func_to_execute:
+    :param out_type:
+    :param out_name:
+    :param args_specifications:
+    :param f_spec:
+    :param method_type:
+    :return:
+    """
+    f_spec = f_spec or inspect.getargspec(to_wrap)
     if f_spec.defaults is None:
         default_values = {}
     else:
@@ -102,76 +112,18 @@ def operation_from_func(f, out_type, out_name, args_specifications, f_spec=None,
 
     def _apply(self, data_store=None):
         this_args = self.get_this_args(data_store)
-        return f(**this_args)
+        return func_to_execute(**this_args)
 
     def __repr__(self):
         this_args = self.get_this_args()
         args = ['%s=%s' % i for i in this_args.iteritems()]
         args = [e if len(e) < 20 else e[:17] + '...' for e in args]
-        return '%s(%s)' % (out_name or f.__name__, ', '.join(args))
+        return '%s(%s)' % (out_name or to_wrap.__name__, ', '.join(args))
 
     cls_attrs = attrs.copy()
-    cls_attrs['func'] = staticmethod(f)
+    cls_attrs['func'] = staticmethod(func_to_execute)
     cls_attrs['_apply'] = _apply
     cls_attrs['__repr__'] = __repr__
     cls_attrs['get_this_args'] = get_this_args
-    cls = type(out_name or f.__name__, (out_type,), cls_attrs)
+    cls = type(out_name or to_wrap.__name__, (out_type,), cls_attrs)
     return cls
-
-
-class CachedFunc(object):
-    memory_cache = {}
-
-    def __init__(self, func, cache_fname):
-        self.func = func
-        self.cache_fname = cache_fname
-
-    def __call__(self, *args, **kwargs):
-        self.try_to_load()
-        if self.in_memory:
-            if VERBOSE: print("memory hit")
-            result = CachedFunc.memory_cache[self.cache_fname]
-        else:
-            lock = FileLock(self.cache_fname + '.lock')
-            print("acquiring lock for %s" % os.path.basename(self.cache_fname))
-            with lock:
-                self.try_to_load()
-                if self.in_memory:
-                    if VERBOSE: print("memory hit")
-                    result = CachedFunc.memory_cache[self.cache_fname]
-                else:
-                    print("building for %s..." % os.path.basename(self.cache_fname))
-                    result = CachedFunc.memory_cache[self.cache_fname] = self.func(*args, **kwargs)
-                    self.save(result)
-
-        return result
-
-    @property
-    def in_memory(self):
-        return self.cache_fname in CachedFunc.memory_cache
-
-    def try_to_load(self):
-        if self.in_memory: return
-        if not os.path.exists(self.cache_fname): return
-
-        try:
-            with open(self.cache_fname) as f:
-                CachedFunc.memory_cache[self.cache_fname] = cPickle.load(f)
-                if True or VERBOSE: print("\tloaded for %s" % os.path.basename(self.cache_fname))
-        except Exception:
-            print("Exception while loading %s" % self.cache_fname)
-            traceback.print_exc()
-            print("Deleting file")
-            if os.path.exists(self.cache_fname): os.unlink(self.cache_fname)
-
-    def save(self, data):
-        with open(self.cache_fname, 'w') as f:
-            cPickle.dump(data, f, 2)
-
-    def clear_memory_cache(self):
-        CachedFunc.memory_cache.pop(self.cache_fname, None)
-
-    def clear_disk_cache(self):
-        self.clear_memory_cache()
-        if os.path.exists(self.cache_fname):
-            os.unlink(self.cache_fname)
