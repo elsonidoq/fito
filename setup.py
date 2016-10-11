f5e9982373cd2176d57ea8291ab72a5226452a04
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2016-10-11 20:28:49 -0300

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='fito',
       version='0.1',
-      description='F.I.T.O',
-      author='',
+      description='fito',
+      author='Pablo Zivic & Bruno Parrino',
       author_email='',
       url='',
-      packages=['fito'],
+      packages=find_packages('.'),
       install_requires=[
             'mmh3',
             'memoized_property'
