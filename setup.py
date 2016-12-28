58400b61ea132824aa09f43dc4acbeae81e2a787
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2016-12-27 22:03:01 -0300

    Learning about tags and releases along the way

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 setup(
     name='fito',
     packages=find_packages(),
-    version='0.1',
+    version='0.2',
     description='fito',
     author='Pablo Zivic & Bruno Parrino',
     author_email='elsonidoq@gmail.com',
     url='https://github.com/elsonidoq/fito',
-    download_url='https://github.com/elsonidoq/fito/tarball/0.1',
+    download_url='https://github.com/elsonidoq/fito/tarball/0.2',
     zip_safe=False,
     install_requires=[
         'mmh3',
