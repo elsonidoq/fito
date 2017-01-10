dd6e2b3264fe86993fb8c9507045393bd2e03bb8
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-01-10 00:13:42 -0300

    More tests for data store

--- tests/data_store.py
+++ tests/data_store.py
@@ -8,6 +8,13 @@ from fito.data_store.mongo import get_collection, global_client
 from spec import get_test_specs
 
 
+def delete(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    elif os.path.isfile(path):
+        os.unlink(path)
+
+
 class TestDataStore(unittest.TestCase):
     def setUp(self):
         self.file_data_store_path = tempfile.mktemp()
@@ -19,25 +26,23 @@ class TestDataStore(unittest.TestCase):
             mongo.MongoHashMap(self.mongo_collection.with_get_cache, get_cache_size=10),
             file.FileDataStore(self.file_data_store_path),
             file.FileDataStore(self.file_data_store_path + '_with_get_cache', get_cache_size=10),
+            file.FileDataStore(self.file_data_store_path + '_dont_split_keys', split_keys=False),
             dict_ds.DictDataStore(),
         ]
 
         # This is just because of MongoHashMap
-        self.test_specs = get_test_specs(only_lists=True)
+        test_specs = get_test_specs(only_lists=True)
+        self.indexed_specs = test_specs[:len(test_specs)/2]
+        self.not_indexed_specs = test_specs[len(test_specs)/2:]
 
         for ds in self.data_stores:
-            for i, spec in enumerate(self.test_specs):
+            for i, spec in enumerate(self.indexed_specs):
                 ds[spec] = i
 
     def tearDown(self):
-        def delete(path):
-            if os.path.isdir(path):
-                shutil.rmtree(path)
-            elif os.path.isfile(path):
-                os.unlink(path)
-
         delete(self.file_data_store_path)
         delete(self.file_data_store_path + '_with_get_cache')
+        delete(self.file_data_store_path + '_dont_split_keys')
 
         self.mongo_collection.drop()
         self.mongo_collection.with_get_cache.drop()
@@ -45,14 +50,18 @@ class TestDataStore(unittest.TestCase):
     def test_iter_items(self):
         for ds in self.data_stores:
             for spec, i in ds.iteritems():
-                assert self.test_specs[i] == spec
+                assert self.indexed_specs[i] == spec
 
     def test_get(self):
         for ds in self.data_stores:
-            for i, spec in enumerate(self.test_specs):
+            for i, spec in enumerate(self.indexed_specs):
                 assert ds[spec] == i
                 assert ds.get(spec) == i
 
+            for spec in self.not_indexed_specs:
+                self.assertRaises(KeyError, ds.get, spec)
+
     def test_keys(self):
         for ds in self.data_stores:
-            assert sorted(ds.iterkeys()) == sorted(self.test_specs)
+            assert sorted(ds.iterkeys()) == sorted(self.indexed_specs)
+
