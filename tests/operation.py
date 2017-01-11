376097de8c9734b0636a7e8f6f706560ecc64910
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-01-11 00:02:35 -0300

    Refactored operation test for it to be usable from other tests

--- tests/operation.py
+++ tests/operation.py
@@ -1,27 +1,72 @@
 import unittest
+from random import Random
 
+from fito import DictDataStore
 from fito import SpecField
+from fito.data_store.base import Get
 from fito.operations.decorate import as_operation
 from fito.operations.operation import Operation
-from fito.specs.base import InvalidSpecInstance, Spec
+from fito.specs.base import InvalidSpecInstance, Spec, BaseSpecField
+
+
+@as_operation()
+def base_case(i=0):
+    return i
+
+
+@as_operation(op=SpecField)
+def f(op, val=1): return val
+
+
+class Test(object):
+    @as_operation(method_type='instance', op=SpecField(base_type=base_case))
+    def op1(self, op, val=1): return 1
+
+    @as_operation(method_type='class', op=SpecField(base_type=base_case))
+    def op2(cls, op): return 1
+
+
+def get_test_operations():
+    res = [
+        base_case(),
+        f(base_case()),
+        Test().op1(base_case()),
+        Test.op2(base_case()),
+    ]
+
+    input = DictDataStore()
+    numbers = [GetNumber(i, input) for i in xrange(10)]
+    for number in numbers: input[number] = number.name * 2
+
+    rnd = Random(42)
+    for i in xrange(10):
+        numbers.append(rnd.choice(numbers) + rnd.choice(numbers))
+
+    return res + numbers
+
+
+class Numeric(Operation):
+    def __add__(self, other):
+        return AddOperation(self, other)
+
+
+class GetNumber(Get, Numeric): pass
+
+
+class AddOperation(Numeric):
+    left = SpecField(0, base_type=Numeric)
+    right = SpecField(1, base_type=Numeric)
+
+    def apply(self, runner):
+        return runner.execute(self.left) + runner.execute(self.right)
+
+    def __repr__(self):
+        return '{} + {}'.format(self.left, self.right)
 
 
 class TestOperation(unittest.TestCase):
-
-    def test_as_operation(self):
-
-        @as_operation(op=SpecField)
-        def f(op, val=1): return val
-
-        class Test(object):
-            @as_operation(method_type='instance', op=SpecField)
-            def op1(self, op, val=1): return 1
-
-            @as_operation(method_type='class', op=SpecField)
-            def op2(cls, op): return 1
-
-        ops = [f, Test().op1, Test.op2]
-        for op in ops:
-            assert issubclass(op, Operation)
-            self.assertRaises(InvalidSpecInstance, op, 1)
-            op(Spec()).apply(None)
+    def test_methods(self):
+        for op in get_test_operations():
+            assert isinstance(op, Operation)
+            op.execute()
+            repr(op)
