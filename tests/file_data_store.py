2aff99a25b7156dc59ff99cca41e4eac0a0d8d77
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-01-11 00:03:36 -0300

    Cleaner file data store testing

--- tests/file_data_store.py
+++ tests/file_data_store.py
@@ -1,25 +1,23 @@
 import tempfile
 import unittest
 
-from fito.data_store.file import FileDataStore, RawSerializer
+from fito.data_store.file import FileDataStore, RawSerializer, PickleSerializer
 from data_store import delete
 from spec import get_test_specs
 
 
 class TestFileDataStore(unittest.TestCase):
     def setUp(self):
-        self.file_data_store_path = tempfile.mktemp()
-
-        self.ds = FileDataStore(self.file_data_store_path, serializer=RawSerializer())
+        self.ds = FileDataStore(tempfile.mktemp(), serializer=RawSerializer())
 
         self.test_specs = get_test_specs(only_lists=True)
 
     def tearDown(self):
-        delete(self.file_data_store_path)
+        delete(self.ds.path)
 
     def test_existing_path(self):
-        assert self.ds.serializer == FileDataStore(self.file_data_store_path).serializer
-        self.assertRaises(RuntimeError, FileDataStore, self.file_data_store_path, serializer=RawSerializer)
+        assert self.ds.serializer == FileDataStore(self.ds.path).serializer
+        self.assertRaises(RuntimeError, FileDataStore, self.ds.path, serializer=PickleSerializer())
 
     def test_clean(self):
         self.ds[self.test_specs[0]] = ""
