92cc99c63570b27d33c265563bb76154bfa55410
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-01-11 00:04:48 -0300

    Bugfixes on dict data store

--- fito/data_store/dict_ds.py
+++ fito/data_store/dict_ds.py
@@ -10,12 +10,13 @@ class DictDataStore(BaseDataStore):
         return self.data.iteritems()
 
     def save(self, name_or_operation, object):
-        self.data[name_or_operation] = object
+        self.data[self._get_spec(name_or_operation)] = object
 
     def _get(self, name_or_operation):
-        if name_or_operation not in self.data: raise KeyError("Spec not found: {}".format(name_or_operation))
+        spec = self._get_spec(name_or_operation)
+        if spec not in self.data: raise KeyError("Spec not found: {}".format(name_or_operation))
 
-        return self.data.get(name_or_operation)
+        return self.data.get(spec)
 
     def iterkeys(self):
         return self.data.iterkeys()
