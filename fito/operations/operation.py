e77075d054aaf0e972cb93797c1148f39afed974
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-01-14 17:23:51 -0300

    Implemented MemoryObject

--- fito/operations/operation.py
+++ fito/operations/operation.py
@@ -1,3 +1,5 @@
+import ctypes
+
 from fito import OperationRunner
 from fito.specs.base import Spec, SpecField, _no_default
 
@@ -11,6 +13,43 @@ class Operation(Spec):
 
     def apply(self, runner):
         raise NotImplementedError()
+
+
+class MemoryObject(Operation):
+    def __init__(self, *args, **kwargs):
+        super(MemoryObject, self).__init__(*args, **kwargs)
+        fields = self.get_primitive_fields()
+
+        if len(fields) != 1:
+            raise RuntimeError("Memory object subclasses must have exactly one primitive field to refer the object")
+
+        self.object_field_name = fields.iterkeys().next()
+
+    @property
+    def object(self):
+        return getattr(self, self.object_field_name)
+
+    def apply(self, runner):
+        return self.object
+
+    def to_dict(self):
+        res = super(MemoryObject, self).to_dict()
+        res[self.object_field_name] = id(self.object)
+        return res
+
+    @classmethod
+    def _from_dict(cls, kwargs):
+        res = super(MemoryObject, cls)._from_dict(kwargs)
+
+        setattr(
+            res,
+            res.object_field_name,
+            ctypes.cast(
+                getattr(res, res.object_field_name),
+                ctypes.py_object
+            ).value
+        )
+        return res
 
 
 def OperationField(pos=None, default=_no_default, base_type=None):
