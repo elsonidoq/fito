e5d8065bd6509b4dd52f0c8b64ce08860efdda40
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-01-14 00:49:20 -0300

    Operation can be autosaving

--- fito/operations/operation.py
+++ fito/operations/operation.py
@@ -1,10 +1,15 @@
 from fito import OperationRunner
-from fito.specs.base import Spec
+from fito.specs.base import Spec, SpecField
 
 
 class Operation(Spec):
+    out_data_store = SpecField(default=None)
+
     def execute(self, runner=None):
-        return self.apply(runner or OperationRunner())
+        res = self.apply(runner or OperationRunner())
+        if self.out_data_store is not None:
+            self.out_data_store[self] = res
+        return res
 
     def apply(self, runner):
         raise NotImplementedError()
