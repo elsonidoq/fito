88885e537ee4cfaaa6a725b82ed4c632700775a3
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-02-03 01:00:07 -0300

    Implemented bind method

--- fito/operations/operation.py
+++ fito/operations/operation.py
@@ -1,9 +1,23 @@
 from fito import OperationRunner
-from fito.specs.base import Spec, SpecField, _no_default, PrimitiveField, load_object, Field, KwargsField
+from fito.specs.base import Spec, SpecField, _no_default, PrimitiveField, load_object, Field, KwargsField, BaseSpecField
 
-class UnbindedField(Field):
+
+class UnbindedField(object):
     pass
 
+
+class BaseUnbindedSpec(BaseSpecField, UnbindedField):
+    pass
+
+
+def UnbindedSpecField(pos=None, default=_no_default, base_type=None, spec_field_subclass=None):
+    spec_field_subclass = spec_field_subclass or BaseUnbindedSpec
+    assert issubclass(spec_field_subclass, BaseUnbindedSpec)
+    return SpecField(pos=pos, default=default, base_type=base_type, spec_field_subclass=spec_field_subclass)
+
+
+class UnbindedPrimitiveField(PrimitiveField, UnbindedField):
+    pass
 
 
 class Operation(Spec):
@@ -25,28 +39,33 @@ class Operation(Spec):
             if isinstance(v, UnbindedField):
                 yield k, v
 
-    def execute(self, runner=None, **extra):
+    def bind(self, *args, **kwargs):
+        fields = dict(self.get_unbinded_fields())
+        return self.copy().initialize(fields, *args, **kwargs)
+
+    def inplace_bind(self, *args, **kwargs):
+        fields = dict(self.get_unbinded_fields())
+        return self.initialize(fields, *args, **kwargs)
+
+    def execute(self, runner=None):
         if self.out_data_store is not None:
             out_data_store = self.out_data_store
         else:
             out_data_store = self.default_data_store
 
         if out_data_store is not None:
-            return out_data_store.get_or_execute(self, runner, **extra)
+            return out_data_store.get_or_execute(self, runner)
         else:
-            return (runner or OperationRunner()).execute(self, **extra)
+            return (runner or OperationRunner()).execute(self)
 
-    def apply(self, runner, context):
+    def apply(self, runner):
         raise NotImplementedError()
-
-    def build_context(self, **extra):
-        return Context.build_from_extra(self, **extra)
 
 
 class MemoryObject(Operation):
     obj = PrimitiveField(0)
 
-    def apply(self, runner, **extra):
+    def apply(self, runner):
         return self.obj
 
     def to_dict(self, include_all=False):
@@ -67,37 +86,7 @@ def OperationField(pos=None, default=_no_default, base_type=None):
 
 class A(Operation):
     a = PrimitiveField(0)
-    b = UnbindedField()
+    b = UnbindedPrimitiveField(0)
 
-    def apply(self, runner, context):
-        return context.b
-
-
-class Context(Spec):
-    target_operation = OperationField(0)
-    extra = KwargsField()
-
-    def set(self, attr, val, type):
-        setattr(self, attr, val)
-        self.specs[attr] = type
-
-    @classmethod
-    def build_from_extra(cls, target_operation, **extra):
-        new_extra = {}
-        for field, field_type in target_operation.get_unbinded_fields():
-            if field in extra:
-                new_extra[field] = extra[field]
-            elif field_type.has_default_value():
-                new_extra[field] = field_type.default
-            else:
-                raise RuntimeError(
-                    'Missing unbinded field "{}" for class {}'.format(field, type(self).__name__)
-                )
-        return cls(target_operation, **new_extra)
-
-    def __getattribute__(self, item):
-        extra = super(Context, self).__getattribute__('extra')
-        if not isinstance(extra, KwargsField) and item in extra:
-            return extra[item]
-        else:
-            return super(Context, self).__getattribute__(item)
+    def apply(self, runner):
+        return self.a + self.b
