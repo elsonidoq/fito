76363eeddce56e51ff0abe68d5943b64f0e8db16
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-02-05 21:04:27 -0300

    Small Bugfix

--- fito/operations/decorate.py
+++ fito/operations/decorate.py
@@ -174,7 +174,8 @@ def operation_from_func(to_wrap, func_to_execute, out_type, out_name, args_speci
                 binded_pos += 1
 
         else:
-            spec = PrimitiveField(len(attrs))
+            spec = PrimitiveField(binded_pos)
+            binded_pos += 1
 
         if arg in default_values: spec.default = default_values[arg]
         attrs[arg] = spec
