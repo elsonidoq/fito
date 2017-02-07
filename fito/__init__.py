ab6bf4bc88d50b156520cc4cef6b0309536dd055
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-02-07 12:17:37 -0300

    fito has __version__

--- fito/__init__.py
+++ fito/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.3.3'
+
 from specs.fields import PrimitiveField, SpecField
 from specs.base import Spec
 from operation_runner import OperationRunner
--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import fito
 
 setup(
     name='fito',
     packages=find_packages(),
-    version='0.3.2',
+    version=fito.__version__,
     description='fito',
     author='Pablo Zivic',
     author_email='elsonidoq@gmail.com',
     url='https://github.com/elsonidoq/fito',
-    download_url='https://github.com/elsonidoq/fito/tarball/0.3.2',
+    download_url='https://github.com/elsonidoq/fito/tarball/' + fito.__version__,
     zip_safe=False,
     install_requires=[
         'mmh3',
