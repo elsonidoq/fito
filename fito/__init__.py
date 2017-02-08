cbc08d6a453dd9d7ecd9f850c5e8c3f142987c33
Pablo Zivic <elsonidoq@gmail.com>
2017-02-08 11:54:32 -0300

    Fixed installation issue

--- fito/__init__.py
+++ fito/__init__.py
@@ -1,8 +1,12 @@
 __version__ = '0.3.3'
 
-from specs.fields import PrimitiveField, SpecField
-from specs.base import Spec
-from operation_runner import OperationRunner
-from operations.decorate import as_operation
-from operations.operation import Operation, OperationField
-from data_store.dict_ds import DictDataStore
+try:
+    from specs.fields import PrimitiveField, SpecField
+    from specs.base import Spec
+    from operation_runner import OperationRunner
+    from operations.decorate import as_operation
+    from operations.operation import Operation, OperationField
+    from data_store.dict_ds import DictDataStore
+except ImportError:
+    # This happens when fito is imported during setup.py
+    pass
