1e2ec27014e7fe9d36c974cacc7ab951bb69f4be
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-03-22 21:38:23 -0300

    Subdiffs well handled that compile to refactors

--- fito/specs/diff.py
+++ fito/specs/diff.py
@@ -1,7 +1,7 @@
 from StringIO import StringIO
 
 from fito import Spec
-from fito.data_store.refactor import StorageRefactor
+from fito.data_store.refactor import StorageRefactor, ChainedRefactor
 from fito.specs.fields import CollectionField, SpecCollection, PrimitiveField
 
 
@@ -15,34 +15,47 @@ class Diff(Spec):
     added_fields = CollectionField()
     removed_fields = CollectionField()
     changed_fields = SpecCollection()
-    subdiffs = SpecCollection()
+    field = PrimitiveField(default=None, help='Specifies that this diff only applies to this field')
 
     @classmethod
-    def build(cls, old_dict, new_dict):
+    def build(cls, old_dict, new_dict, field=None):
         added_fields = {k: v for k, v in new_dict.iteritems() if k not in old_dict}
         removed_fields = [k for k in old_dict if k not in new_dict]
 
         changed_fields = {}
-        subdiffs = {}
+        subdiffs = []
         for k in old_dict:
             if k not in new_dict: continue
 
             old_v = old_dict[k]
             new_v = new_dict[k]
             if isinstance(old_v, dict) and 'type' in old_v and isinstance(new_v, dict) and 'type' in new_v:
+                # TODO this fails if a param starts being a spec, or a param stops being a spec
                 # i.e. old_v and new_v refer to specs
-                subdiffs[k] = Diff.build(old_v, new_v)
+
+                if field is None:
+                    subdiff_field = k
+                else:
+                    subdiff_field = '{}.{}'.format(field, k)
+                subdiff = Diff.build(old_v, new_v, field=subdiff_field)
+                subdiffs.append(subdiff)
 
             elif old_v != new_v:
                 changed_fields[k] = FieldChange(old_v, new_v)
 
-        return Diff(
+        diff = Diff(
             spec_type=old_dict['type'],
             added_fields=added_fields,
             removed_fields=removed_fields,
             changed_fields=changed_fields,
-            subdiffs=subdiffs
+            field=field
         )
+
+        if subdiffs:
+            subdiffs.insert(0, diff)
+            return ChainedDiff(subdiffs)
+        else:
+            return diff
 
     def create_refactor(self):
         res = StorageRefactor()
@@ -55,33 +68,62 @@ class Diff(Spec):
         for field, field_change in self.changed_fields.iteritems():
             res = res.add_field(self.spec_type, field_change.new_value)
 
-        for field, subdiff in self.subdiffs:
-            res = StorageRefactor()
+        if self.field:
+            res = res.project(self.field)
 
+        return res
 
     def __repr__(self):
         res = StringIO()
-        template = '{}:\n\t{}'
+        if self.field is None:
+            template = '{}:\n\t{}'
+            join_template = '\n\t'
+        else:
+            res.write("On field '{}'\n".format(self.field))
+            template = '\t{}:\n\t\t{}'
+            join_template = '\n\t\t'
 
         if self.added_fields:
-            res.write(template.format('Added fields', '\n\t'.join(self.added_fields)))
+            res.write(
+                template.format(
+                    'Added fields',
+                    join_template.join(
+                        '{}:\t{}'.format(k, v) for k, v in self.added_fields.iteritems()
+                    )
+                )
+            )
 
         if self.removed_fields:
             if res.len > 0: res.write('\n\n')
-            res.write(template.format('Removed fields', '\n\t'.join(self.removed_fields)))
+            res.write(template.format('Removed fields', join_template.join(self.removed_fields)))
 
         if self.changed_fields:
             if res.len > 0: res.write('\n\n')
             res.write('Changed fields:\n')
             res.write('\t{:<20} {:<20} {:<20}'.format('field', 'from value', 'to value'))
             for field, field_change in self.changed_fields.iteritems():
-                res.write('\n\t{:<20} {:<20} {:<20}'.format(field, field_change.original_value, field_change.new_value))
+                res.write('{}{:<20} {:<20} {:<20}'.format(join_template, field, field_change.original_value, field_change.new_value))
 
-        if self.subdiffs:
-            if res.len > 0: res.write('\n\n')
-            res.write('\nSub diffs:\n')
-            for field, subdiff in self.subdiffs.iteritems():
-                res.write('\tField: {}\n'.format(field))
-                res.write('\t' + repr(subdiff).replace('\n', '\n\t'))
+        return res.getvalue()
+
+
+class ChainedDiff(Spec):
+    subdiffs = SpecCollection(0)
+
+    def create_refactor(self):
+        refactors = []
+
+        for subdiff in self.subdiffs:
+            refactors.append(subdiff.create_refactor())
+
+        return ChainedRefactor(*refactors)
+
+    def __repr__(self):
+        res = StringIO()
+
+        for i, diff in enumerate(self.subdiffs):
+            res.write(repr(diff))
+            if i < len(self.subdiffs):
+                res.write('\n\n')
 
         return res.getvalue()
