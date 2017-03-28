ffa60ad9fc3f752cf2849e03236feefa2a1ea2fd
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2017-03-28 00:09:21 -0300

    Interactive rehash example

---
+++ examples/interactive_rehash/first_experiment.py
@@ -0,0 +1,44 @@
+# Ok, you've just came to the idea of doing an experiment
+from fito import Operation
+from fito.data_store import FileDataStore
+from fito.specs.fields import NumericField
+
+
+# So you code your experiment class that runs depending on some parameter
+class Experiment(Operation):
+    some_parameter = NumericField(0)
+
+    def apply(self, runner):
+        print "Running Experiment({})...".format(self.some_parameter)
+        return self.some_parameter * 2
+
+    def __repr__(self):
+        return 'Experiment({})'.format(self.some_parameter)
+
+
+def perform_some_runs(data_store, experiments):
+    for exp in experiments:
+        # I wrote this extra code so it's easier to follow
+        # However this "autocaching" can be also performed by either:
+        # - setting the default_data_store class field on the class definition
+        # - creating the experiment as follows: Experiment(p, out_data_store=data_store)
+        if exp not in data_store:
+            data_store[exp] = exp.execute()
+        else:
+            print "Skipping {}".format(exp)
+
+# Let's perform some runs
+if __name__ == '__main__':
+    ds = FileDataStore('caches')
+    # Defining the Experiment class in the same file than the script isn't a very good idea
+    #
+    # Because when we hash the experiments, the inspect module is going to tell fito that
+    # the class was defined on the __main__ module, thus you won't be able to import
+    # this class and find it on a data store.
+    #
+    # Of course, that raises a warning from fito :)
+    # I'm using that behaviour for the sake of this example, so I can generate misses on second_experiment.py
+    experiments = [Experiment(p) for p in xrange(3)]
+    perform_some_runs(ds, experiments)
+
+
---
+++ examples/interactive_rehash/second_experiment.py
@@ -0,0 +1,39 @@
+# Now you realized that you need a new hyperparameter
+# Luckily there's a default value for that parameter that generates the same behaviour as before
+# so you don't want to loose your cached computations
+
+from fito import Operation
+from fito.data_store import FileDataStore
+from fito.specs.fields import NumericField
+
+
+# So you code your experiment class that runs depending on some parameter
+class Experiment(Operation):
+    some_parameter = NumericField(0)
+    new_parameter = NumericField(1, default=2)
+
+    def apply(self, runner):
+        print "Running Experiment({})...".format(self.some_parameter)
+        # new_parameter's default value is two, like in the previous experiment
+        return self.some_parameter * self.new_parameter
+
+    def __repr__(self):
+        return 'Experiment({}, {})'.format(self.some_parameter, self.new_parameter)
+
+# Don't want to rewrite that code
+from first_experiment import perform_some_runs
+
+# Let's perform some runs
+if __name__ == '__main__':
+    ds = FileDataStore('caches')
+    # Defining the Experiment class in the same file than the script isn't a very good idea
+    #
+    # Because when we hash the experiments, the inspect module is going to tell fito that
+    # the class was defined on the __main__ module, thus you won't be able to import
+    # this class and find it on a data store.
+    #
+    # Of course, that raises a warning from fito :)
+    # I'm using that behaviour for the sake of this example, so I can generate misses on second_experiment.py
+    experiments = [Experiment(p, q) for p in xrange(3) for q in xrange(2, 4)]
+    perform_some_runs(ds, experiments)
+
